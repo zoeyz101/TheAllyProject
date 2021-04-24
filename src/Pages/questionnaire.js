ddb53247d84ca0b8a3d17bf4f76f108040c0e62f
Zoey Zhang <zoeyzhang2001@gmail.com>
2021-04-24 19:25:35 -0400

    adding questions constants

--- src/Pages/questionnaire.js
+++ src/Pages/questionnaire.js
@@ -1,9 +1,15 @@
+import {Row,Col, Container} from react-bootstrap
 import styles from './questionnaire.scss'
 
 export const Questionnaire = () => {
     return(
-        <div>
-            test
-        </div>
+        <Container>
+            <Row>
+                {question}
+            </Row>
+            <Row>
+               <Answer/>
+            </Row>
+        </Container>
     )
 }
